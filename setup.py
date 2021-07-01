6dbe6525ef5e4cec854ed0d548c32cae4e7911f5
Ryleigh Davis <ryleighdavis@Ryleighs-MacBook-Pro-2.local>
2021-06-30 18:05:23 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'A spectrum viewing tool'
+LONG_DESCRIPTION = 'A gui based tool to view and bin spectra of planetary surfaces by selecting regions on a map.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="specview", 
+        version=VERSION,
+        url="https://github.com/RyleighDavis/specview",
+        author="M. Ryleigh Davis",
+        author_email="<rdavis@caltech.edu>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'specview', 'spectral viewing tool'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+        ]
+)
